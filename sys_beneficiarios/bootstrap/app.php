5d43b886b2286b28b5cf9001027c83a2db9bc461
ManuelRuiz27 <manuel_ruiz27@live.com>
2026-01-05 14:17:01 -0600

    Trust proxies for HTTPS behind Render

--- sys_beneficiarios/bootstrap/app.php
+++ sys_beneficiarios/bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->appendToGroup('api', ProblemJsonMiddleware::class);
 
         $middleware->alias([
